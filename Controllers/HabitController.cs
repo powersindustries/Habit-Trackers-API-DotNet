7944122146dc2f65e8b6638ee2e954d5ed970691
powersindustries <lp.accounts@fastmail.com>
2024-02-13 10:56:49 -0600

    Updated response status codes for HabitController.

--- Controllers/HabitController.cs
+++ Controllers/HabitController.cs
@@ -13,6 +13,8 @@ namespace Controllers
         // GET - All Habit data.
         // -----------------------------------------------------
         [HttpGet("get/all")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
         public IActionResult GetAllHabits()
         {
             List<Models.Habit> habitListData = Helpers.SqlHelpers.GetAllHabits();
@@ -29,6 +31,8 @@ namespace Controllers
         // GET - Habit data by ID.
         // -----------------------------------------------------
         [HttpGet("get/{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         public IActionResult GetByHabitID(string inID)
         {
             string hashedId = Helpers.Helpers.StringToHash(inID);
@@ -36,7 +40,7 @@ namespace Controllers
             Models.Habit habitData = Helpers.SqlHelpers.GetHabitByHashedID(hashedId);
             if (habitData.IsEmpty())
 	        {
-                return StatusCode(500, "Habit ID (" + inID + ") doesnt exist.");
+                return StatusCode(400, "Habit ID (" + inID + ") doesnt exist.");
             } 
 
 	        return Ok(habitData);
@@ -47,11 +51,14 @@ namespace Controllers
         // POST - Create new Habit data.
         // -----------------------------------------------------
         [HttpPost("add")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
         public IActionResult AddNewHabitData([FromBody] Models.Habit inHabitData)
         {
             if (inHabitData.IsEmpty())
             { 
-                return StatusCode(500, "Body habit object is empty.");
+                return StatusCode(400, "Body habit object is empty.");
 	        }
 
             inHabitData.Id = Helpers.Helpers.StringToHash(inHabitData.Id);
@@ -69,13 +76,15 @@ namespace Controllers
         // DELETE -  Habit data by ID.
         // -----------------------------------------------------
         [HttpDelete("delete/{id}")]
+        [ProducesResponseType(202)]
+        [ProducesResponseType(404)]
         public IActionResult DeleteHabitByID(string id)
         {
             string hashedID = Helpers.Helpers.StringToHash(id);
 
             if (!Helpers.SqlHelpers.DeleteHabit(hashedID))
             {
-                return StatusCode(500, "Failed to create new Habit object.");
+                return StatusCode(404, "Failed to delete Habit object with id (" + id + ").");
 	        }
 
             return Ok("Success");
